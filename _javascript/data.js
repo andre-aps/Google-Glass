b6ac21ded0452928ed591410a34c8d9e00e133d5
Andre <andre.gmaps@gmail.com>
2018-05-22 11:13:08 -0300

    Javascript

---
+++ _javascript/data.js
@@ -0,0 +1,3 @@
+dayName = new Array ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado")
+monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+now = new Date
